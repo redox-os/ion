4c62268c50bb12f7420749927618bf77fa013169
Michael Gattozzi <mgattozzi@gmail.com>
2015-10-22 08:19:20 -0400

    Suppressed unused variable. It's unused considering read line stores the result into a buffer

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,8 @@ use command::*;
 pub fn repl() {
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
-        Ok(n) => {
-            let out_wrap = run(input.trim().split_whitespace().collect::<Vec<&str>>());
+        Ok(_) => {
+            let out_wrap = run(input.trim().split_whitespace().collect::<Vec<&str>>().as_slice());
             if out_wrap.is_some() {
                 let out = out_wrap.unwrap();
                 if out.stdout.is_empty() {
