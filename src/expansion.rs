408412434facb3ae543efcfc0a5f42309a21a001
stratact <stratact1@gmail.com>
2016-01-24 04:38:11 -0800

    Have the user decide when to allocate a String or not

--- src/expansion.rs
+++ src/expansion.rs
@@ -3,24 +3,24 @@ use super::Variables;
 
 pub fn expand_variables(mut jobs: Vec<Job>, variables: &Variables) -> Vec<Job> {
     for mut job in &mut jobs {
-        job.command = expand_string(&job.command, variables);
+        job.command = expand_string(&job.command, variables).to_string();
         job.args = job.args
                       .iter()
-                      .map(|original: &String| expand_string(&original, variables))
+                      .map(|original: &String| expand_string(&original, variables).to_string())
                       .collect();
     }
     jobs
 }
 
 #[inline]
-fn expand_string(original: &str, variables: &Variables) -> String {
+fn expand_string<'a>(original: &'a str, variables: &'a Variables) -> &'a str {
     if original.starts_with("$") {
         if let Some(value) = variables.get(&original[1..]) {
-            value.clone()
+            &value
         } else {
-            String::new()
+            ""
         }
     } else {
-        original.to_string()
+        original
     }
 }
