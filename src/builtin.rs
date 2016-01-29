8072a13a32884ed4a22fab51b0b37473879b2fd4
Skyler Berg <skylertheberg@gmail.com>
2016-01-28 22:36:11 -0800

    Move expansion logic into variables struct

--- src/builtin.rs
+++ src/builtin.rs
@@ -1,9 +1,7 @@
-use std::io::{stdout, Write};
 use std::env;
 use std::process;
 
 use super::Shell;
-use super::input_editor::readln;
 
 pub fn cd(args: &[String]) {
     match args.get(1) {
@@ -13,23 +11,6 @@ pub fn cd(args: &[String]) {
             }
         }
         None => println!("No path given"),
-    }
-}
-
-pub fn read(args: &[String], shell: &mut Shell) {
-    let mut out = stdout();
-    for i in 1..args.len() {
-        if let Some(arg_original) = args.get(i) {
-            let arg = arg_original.trim();
-            print!("{}=", arg);
-            if let Err(message) = out.flush() {
-                println!("{}: Failed to flush stdout", message);
-            }
-            if let Some(value_original) = readln() {
-                let value = value_original.trim();
-                shell.set_var(arg, value);
-            }
-        }
     }
 }
 
@@ -48,7 +29,7 @@ pub fn run(args: &[String], shell: &mut Shell) {
             match child.wait() {
                 Ok(status) => {
                     if let Some(code) = status.code() {
-                        shell.set_var("?", &format!("{}", code));
+                        shell.variables.set_var("?", &format!("{}", code));
                     } else {
                         println!("{}: No child exit code", path);
                     }
---
+++ src/variables.rs
@@ -0,0 +1,74 @@
+use std::collections::BTreeMap;
+use std::io::{stdout, Write};
+
+use super::peg::Job;
+use super::input_editor::readln;
+
+pub struct Variables {
+    variables: BTreeMap<String, String>,
+}
+
+impl Variables {
+    pub fn new() -> Variables {
+        Variables { variables: BTreeMap::new() }
+    }
+
+    pub fn read(&mut self, args: &[String]) {
+        let mut out = stdout();
+        for i in 1..args.len() {
+            if let Some(arg_original) = args.get(i) {
+                let arg = arg_original.trim();
+                print!("{}=", arg);
+                if let Err(message) = out.flush() {
+                    println!("{}: Failed to flush stdout", message);
+                }
+                if let Some(value_original) = readln() {
+                    let value = value_original.trim();
+                    self.set_var(arg, value);
+                }
+            }
+        }
+    }
+
+    pub fn let_(&mut self, args: &[String]) {
+        if args.len() <= 1 {
+            for (key, value) in self.variables.iter() {
+                println!("{}={}", key, value);
+            }
+        }
+        // TODO use let to set variables if they are provided as arguments
+    }
+
+    pub fn set_var(&mut self, name: &str, value: &str) {
+        if !name.is_empty() {
+            if value.is_empty() {
+                self.variables.remove(&name.to_string());
+            } else {
+                self.variables.insert(name.to_string(), value.to_string());
+            }
+        }
+    }
+
+    pub fn expand_variables(&self, jobs: &mut [Job]) {
+        for mut job in &mut jobs[..] {
+            job.command = self.expand_string(&job.command).to_string();
+            job.args = job.args
+                          .iter()
+                          .map(|original: &String| self.expand_string(&original).to_string())
+                          .collect();
+        }
+    }
+
+    #[inline]
+    fn expand_string<'a>(&'a self, original: &'a str) -> &'a str {
+        if original.starts_with("$") {
+            if let Some(value) = self.variables.get(&original[1..]) {
+                &value
+            } else {
+                ""
+            }
+        } else {
+            original
+        }
+    }
+}
