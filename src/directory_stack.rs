78f9d6169be01755aa7917f6a5de9cb28b7e62a0
Skyler Berg <skylertheberg@gmail.com>
2016-02-07 17:25:26 -0800

    Add `cd -` and fix bug in `popd`
    This patch was intended to add `cd -`, but in the course of adding the
    feature I found a critical flaw in `popd` that must have been introduced
    when I switched to a VecDeque.

--- src/directory_stack.rs
+++ src/directory_stack.rs
@@ -25,17 +25,16 @@ impl DirectoryStack {
     pub fn popd<I: IntoIterator>(&mut self, _: I) -> i32
         where I::Item: AsRef<str>
     {
-        if self.dirs.len() < 2 {
-            println!("Directory stack is empty");
-            return FAILURE;
-        }
-        if let Some(dir) = self.dirs.get(self.dirs.len() - 2) {
+        if let Some(dir) = self.get_previous_dir() {
             if let Err(err) = set_current_dir(dir) {
                 println!("{}: Failed to switch to directory {}", err, dir.display());
                 return FAILURE;
             }
+        } else {
+            println!("Directory stack is empty");
+            return FAILURE;
         }
-        self.dirs.pop_back();
+        self.dirs.pop_front();
         self.print_dirs();
         SUCCESS
     }
@@ -58,24 +57,50 @@ impl DirectoryStack {
     {
         if let Some(dir) = args.into_iter().nth(1) {
             let dir = dir.as_ref();
-            self.change_and_push_dir(dir)
-        } else {
-            if let Some(home) = home_dir() {
-                if let Some(home) = home.to_str() {
-                    self.change_and_push_dir(home)
-                } else {
-                    println!("Failed to convert home directory to str");
-                    FAILURE
-                }
+            if dir == "-" {
+                self.switch_to_previous_directory()
             } else {
-                println!("Failed to get home directory");
-                FAILURE
+                self.change_and_push_dir(dir)
             }
+        } else {
+            self.switch_to_home_directory()
         }
     }
 
-    pub fn change_and_push_dir(&mut self, dir: &str) -> i32
-    {
+    fn switch_to_home_directory(&mut self) -> i32 {
+        if let Some(home) = home_dir() {
+            if let Some(home) = home.to_str() {
+                self.change_and_push_dir(home)
+            } else {
+                println!("Failed to convert home directory to str");
+                FAILURE
+            }
+        } else {
+            println!("Failed to get home directory");
+            FAILURE
+        }
+    }
+
+    fn switch_to_previous_directory(&mut self) -> i32 {
+        if let Some(prev) = self.get_previous_dir()
+                                .map(|path| path.to_string_lossy().to_string()) {
+            println!("{}", prev);
+            self.change_and_push_dir(&prev)
+        } else {
+            println!("No previous directory to switch to");
+            FAILURE
+        }
+    }
+
+    fn get_previous_dir(&self) -> Option<&PathBuf> {
+        if self.dirs.len() < 2 {
+            None
+        } else {
+            self.dirs.get(1)
+        }
+    }
+
+    pub fn change_and_push_dir(&mut self, dir: &str) -> i32 {
         match (set_current_dir(dir), current_dir()) {
             (Ok(()), Ok(cur_dir)) => {
                 self.push_dir(cur_dir);
