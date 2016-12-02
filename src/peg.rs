c8d48cf55007d6baa1bcef71fcc9d0bd90c9f77d
Michael Aaron Murphy <mmstickman@gmail.com>
2016-12-02 12:36:43 -0500

    Piping Grammar Fixes & Bit-Twiddling Optimizations This will fix the issue with pipe delimiters without surrounding spaces and make the grammar simpler. In addition, this will further optimize the existing bit-twiddling in the grammar and bit-twiddle the pipleline collection mechanism.

--- src/peg.rs
+++ src/peg.rs
@@ -104,12 +104,16 @@ const SINGLE_QUOTE: u8 = 2;
 const DOUBLE_QUOTE: u8 = 4;
 const WHITESPACE:   u8 = 8;
 const COMMENT:      u8 = 16;
-const PROCESS_ZERO: u8 = 32;
 const PROCESS_ONE:  u8 = 64;
 const PROCESS_TWO:  u8 = 128;
 
+// Only valid if `SINGLE_QUOTE` and `DOUBLE_QUOTE` are not enabled
+const PROCESS_VAL:  u8 = 255 ^ (BACKSLASH + WHITESPACE + COMMENT + 32);
 
-/// An iterator that splits a given command into pipelines
+// Determines if the character is not quoted and isn't process matched. `flags & IS_VALID` returns 0 if true
+const IS_VALID: u8 = 255 ^ (BACKSLASH + WHITESPACE + COMMENT);
+
+/// An iterator that splits a given command into pipelines -- individual command statements delimited by ';'.
 struct PipelineIterator<'a> {
     match_str:       &'a str,
     flags:           u8,
@@ -122,7 +126,7 @@ impl<'a> PipelineIterator<'a> {
     fn new(match_str: &'a str) -> PipelineIterator<'a> {
         PipelineIterator {
             match_str:       match_str,
-            flags:           if match_str.chars().next().unwrap() == '#' { COMMENT + PROCESS_ZERO } else { PROCESS_ZERO },
+            flags:           if match_str.chars().next().unwrap() == '#' { COMMENT } else { 0 },
             index_start:     0,
             index_end:       0,
             white_pos:       0,
@@ -146,21 +150,10 @@ impl<'a> Iterator for PipelineIterator<'a> {
                     '\\'                                                    => self.flags |= BACKSLASH,
                     '\'' if self.flags & (PROCESS_TWO + DOUBLE_QUOTE) == 0  => self.flags ^= SINGLE_QUOTE,
                     '"'  if self.flags & (PROCESS_TWO + SINGLE_QUOTE) == 0  => self.flags ^= DOUBLE_QUOTE,
-                    '$'  if self.flags & SINGLE_QUOTE == 0 && self.flags & PROCESS_ZERO != 0 => {
-                        self.flags |= PROCESS_ONE;
-                        self.flags &= 255 ^ PROCESS_ZERO;
-                    },
-                    '('  if self.flags & SINGLE_QUOTE == 0 && self.flags & PROCESS_ONE != 0 => {
-                        self.flags |= PROCESS_TWO;
-                        self.flags &= 255 ^ PROCESS_ONE;
-                    },
-                    ')'  if self.flags & SINGLE_QUOTE == 0 && self.flags & PROCESS_TWO != 0 => {
-                        self.flags |= PROCESS_ZERO;
-                        self.flags &= 255 ^ PROCESS_TWO;
-                    },
-                    '#'  if self.flags & (PROCESS_TWO + SINGLE_QUOTE + DOUBLE_QUOTE) == 0 &&
-                        self.flags & WHITESPACE != 0 =>
-                    {
+                    '$'  if self.flags & PROCESS_VAL == 0                   => self.flags |= PROCESS_ONE,
+                    '('  if self.flags & PROCESS_VAL == PROCESS_ONE         => self.flags ^= PROCESS_ONE + PROCESS_TWO,
+                    ')'  if self.flags & PROCESS_VAL == PROCESS_TWO         => self.flags &= 255 ^ PROCESS_TWO,
+                    '#'  if self.flags == WHITESPACE => {
                         if self.index_start < self.white_pos {
                             let command = &self.match_str[self.index_start..self.white_pos];
                             self.index_start = self.index_end + 1;
@@ -172,14 +165,14 @@ impl<'a> Iterator for PipelineIterator<'a> {
                             self.flags |= COMMENT;
                         }
                     },
-                    ' ' | '\t' if self.flags & (PROCESS_TWO + SINGLE_QUOTE + DOUBLE_QUOTE) == 0 => {
+                    ' ' | '\t' if self.flags & IS_VALID == 0 => {
                         if self.index_start == self.index_end { self.index_start += 1; }
                         self.flags |= WHITESPACE;
                         if self.white_pos == 0 { self.white_pos = self.index_end; }
                         self.index_end += 1;
                         continue
                     },
-                    ';' | '\n' | '\r' if self.flags & (PROCESS_TWO + SINGLE_QUOTE + DOUBLE_QUOTE) == 0 => {
+                    ';' | '\n' | '\r' if self.flags & IS_VALID == 0 => {
                         if self.index_start == self.index_end {
                             self.index_start += 1;
                             self.flags |= WHITESPACE;
@@ -197,10 +190,7 @@ impl<'a> Iterator for PipelineIterator<'a> {
                         self.index_end += 1;
                         continue
                     },
-                    _ if self.flags & PROCESS_TWO == 0 => {
-                        self.flags |= PROCESS_ZERO;
-                        self.flags &= 255 ^ (PROCESS_ONE + PROCESS_TWO);
-                    },
+                    _ if (self.flags >> 6 != 2) => self.flags &= 255 ^ (PROCESS_ONE + PROCESS_TWO),
                     _ => (),
                 }
                 self.flags &= 255 ^ WHITESPACE;
@@ -220,6 +210,208 @@ impl<'a> Iterator for PipelineIterator<'a> {
         } else {
             None
         }
+    }
+}
+
+#[derive(PartialEq)]
+enum RedirMode { False, Stdin, Stdout, StdoutAppend }
+
+/// Parses each individual pipeline, separating arguments, pipes, background tasks, and redirections.
+pub fn collect_pipelines(pipelines: &mut Vec<Pipeline>, possible_error: &mut Option<&str>, command: &str) {
+    for args in PipelineIterator::new(command) {
+        let mut jobs: Vec<Job> = Vec::new();
+        let mut args_iter = args.chars();
+        let (mut index, mut arg_start) = (0, 0);
+        let mut flags = 0u8; // (backslash, single_quote, double_quote, x, x, x, process_one, process_two)
+
+        let mut arguments: Vec<String> = Vec::new();
+
+        let (mut in_file, mut out_file) = (None, None);
+        let mut mode = RedirMode::False;
+
+        macro_rules! redir_check {
+            ($file:ident, $name:ident, $is_append:expr) => {{
+                if $file.is_none() {
+                    if $name.is_empty() {
+                        *possible_error = Some("missing standard output file argument after '>'");
+                    } else {
+                        $file = Some(Redirection { file: $name, append: $is_append });
+                    }
+                }
+            }}
+        }
+
+        'outer: loop {
+
+            macro_rules! redir_found {
+                ($kind:expr) => {{ mode = $kind; index += 1; arg_start = index; continue 'outer }}
+            }
+
+            macro_rules! job_found {
+                ($is_background:expr) => {{
+                    if arguments.is_empty() {
+                        jobs.push(Job::new(vec![args[arg_start..index].to_owned()], $is_background));
+                    } else {
+                        jobs.push(Job::new(arguments.clone(), $is_background));
+                        arguments.clear();
+                    }
+                    arg_start = index + 1;
+                }}
+            }
+
+            match mode {
+                RedirMode::False => {
+                    while let Some(character) = args_iter.next() {
+                        match character {
+                            _ if flags & BACKSLASH != 0                => flags ^= BACKSLASH,
+                            '\\'                                       => flags ^= BACKSLASH,
+                            '$'  if flags & PROCESS_VAL == 0           => flags |= PROCESS_ONE,
+                            '('  if flags & PROCESS_VAL == PROCESS_ONE => flags ^= PROCESS_ONE + PROCESS_TWO,
+                            ')'  if flags & PROCESS_VAL == PROCESS_TWO => flags &= 255 ^ PROCESS_TWO,
+                            '\''                                       => flags ^= SINGLE_QUOTE,
+                            '"'                                        => flags ^= DOUBLE_QUOTE,
+                            ' ' | '\t' if (flags & IS_VALID == 0) => {
+                                if arg_start != index {
+                                    arguments.push(args[arg_start..index].to_owned());
+                                    arg_start = index + 1;
+                                } else {
+                                    arg_start += 1;
+                                }
+                            },
+                            '|' if (flags & IS_VALID == 0) => job_found!(false),
+                            '&' if (flags & IS_VALID == 0) => job_found!(true),
+                            '>' if (flags & IS_VALID == 0) => redir_found!(RedirMode::Stdout),
+                            '<' if (flags & IS_VALID == 0) => redir_found!(RedirMode::Stdin),
+                            _   if (flags >> 6 != 2)       => flags &= 255 ^ (PROCESS_ONE + PROCESS_TWO),
+                            _ => (),
+                        }
+                        index += 1;
+                    }
+                    break 'outer
+                },
+                RedirMode::Stdout | RedirMode::StdoutAppend => {
+                    match args_iter.next() {
+                        Some(character) => if character == '>' { mode = RedirMode::StdoutAppend; },
+                        None => {
+                            *possible_error = Some("missing standard output file argument after '>'");
+                            break 'outer
+                        }
+                    }
+
+                    let mut stdout_file = String::new();
+                    let mut found_file = false;
+                    while let Some(character) = args_iter.next() {
+                        if found_file {
+                            if character == '<' {
+                                if in_file.is_some() {
+                                    break 'outer
+                                } else {
+                                    mode = RedirMode::Stdin;
+                                    continue 'outer
+                                }
+                            }
+                        } else {
+                            match character {
+                                _ if flags & BACKSLASH != 0 => {
+                                    stdout_file.push(character);
+                                    flags ^= BACKSLASH;
+                                }
+                                '\\' => flags ^= BACKSLASH,
+                                ' ' | '\t' | '|' if stdout_file.is_empty() => (),
+                                ' ' | '\t' | '|' => {
+                                    found_file = true;
+                                    out_file = Some(Redirection {
+                                        file: stdout_file.clone(),
+                                        append: mode == RedirMode::StdoutAppend
+                                    });
+                                },
+                                '<' if stdout_file.is_empty() => {
+                                    *possible_error = Some("missing standard output file argument after '>'");
+                                    break 'outer
+                                }
+                                '<' => {
+                                    out_file = Some(Redirection {
+                                        file: stdout_file.clone(),
+                                        append: mode == RedirMode::StdoutAppend
+                                    });
+
+                                    if in_file.is_some() {
+                                        break 'outer
+                                    } else {
+                                        mode = RedirMode::Stdin;
+                                        continue 'outer
+                                    }
+                                },
+                                _ => stdout_file.push(character),
+                            }
+                        }
+                    }
+
+                    redir_check!(out_file, stdout_file, mode == RedirMode::StdoutAppend);
+
+                    break 'outer
+                },
+                RedirMode::Stdin => {
+                    let mut stdin_file = String::new();
+                    let mut found_file = false;
+
+                    while let Some(character) = args_iter.next() {
+                        if found_file {
+                            if character == '>' {
+                                if out_file.is_some() {
+                                    break 'outer
+                                } else {
+                                    mode = RedirMode::Stdout;
+                                    continue 'outer
+                                }
+                            }
+                        } else {
+                            match character {
+                                _ if flags & BACKSLASH != 0 => {
+                                    stdin_file.push(character);
+                                    flags ^= BACKSLASH;
+                                }
+                                '\\' => flags ^= BACKSLASH,
+                                ' ' | '\t' | '|' if stdin_file.is_empty() => (),
+                                ' ' | '\t' | '|' => {
+                                    found_file = true;
+                                    in_file = Some(Redirection { file: stdin_file.clone(), append: false });
+                                },
+                                '>' if stdin_file.is_empty() => {
+                                    *possible_error = Some("missing standard input file argument after '<'");
+                                    break 'outer
+                                }
+                                '>' => {
+                                    in_file = Some(Redirection { file: stdin_file.clone(), append: false });
+
+                                    if out_file.is_some() {
+                                        break 'outer
+                                    } else {
+                                        mode = RedirMode::Stdin;
+                                        continue 'outer
+                                    }
+                                },
+                                _ => stdin_file.push(character),
+                            }
+                        }
+                    }
+
+                    redir_check!(in_file, stdin_file, false);
+
+                    break 'outer
+                }
+            }
+        }
+
+        if arg_start != index {
+            arguments.push(args[arg_start..index].to_owned());
+        }
+
+        if !arguments.is_empty() {
+            jobs.push(Job::new(arguments, false));
+        }
+
+        pipelines.push(Pipeline::new(jobs, in_file, out_file));
     }
 }
 
@@ -268,6 +460,28 @@ mod tests {
             assert_eq!("echo", jobs[0].args[0]);
             assert_eq!("\"$A\"", jobs[0].args[1]);
             assert_eq!(2, jobs[0].args.len());
+        } else {
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn pipes_without_spaces() {
+        if let Statement::Pipelines(mut pipelines) = parse("ls|cat; ls | cat|cat") {
+            let jobs = pipelines.remove(0).jobs;
+            println!("{:?}", jobs);
+            assert_eq!("ls", jobs[0].args[0]);
+            assert_eq!(1, jobs[0].args.len());
+            assert_eq!("cat", jobs[1].args[0]);
+            assert_eq!(1, jobs[1].args.len());
+            let jobs = pipelines.remove(0).jobs;
+            println!("{:?}", jobs);
+            assert_eq!("ls", jobs[0].args[0]);
+            assert_eq!(1, jobs[0].args.len());
+            assert_eq!("cat", jobs[1].args[0]);
+            assert_eq!(1, jobs[1].args.len());
+            assert_eq!("cat", jobs[2].args[0]);
+            assert_eq!(1, jobs[2].args.len());
         } else {
             assert!(false);
         }
