25d5e900d33d69369740802ede5f596c063f690f
Hunter Goldstein <hunter.d.goldstein@gmail.com>
2017-06-20 13:46:49 -0400

    Hide tokio behind gate for redox

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@
 #[macro_use]
 extern crate bitflags;
 extern crate fnv;
-extern crate futures;
 extern crate glob;
 extern crate liner;
 extern crate smallvec;
 extern crate smallstring;
-extern crate tokio_core;
-extern crate tokio_signal;
+
+#[cfg(not(target_os = "redox"))] extern crate futures;
+#[cfg(not(target_os = "redox"))] extern crate tokio_core;
+#[cfg(not(target_os = "redox"))] extern crate tokio_signal;
 
 #[cfg(all(unix, not(target_os = "redox")))]
 extern crate users as users_unix;
@@ -30,11 +31,13 @@ use std::io::{stderr, Write, ErrorKind};
 use builtins::Builtin;
 use shell::Shell;
 
-use tokio_core::reactor::Core;
-use futures::{Future, Stream};
+#[cfg(not(target_os = "redox"))] use tokio_core::reactor::Core;
+#[cfg(not(target_os = "redox"))] use futures::{Future, Stream};
+
 use std::sync::mpsc;
 use std::thread;
 
+#[cfg(not(target_os = "redox"))]
 fn main() {
     let (sigint_tx, sigint_rx) = mpsc::channel();
 
@@ -70,4 +73,29 @@ fn main() {
         Ok(())
     });
     core.run(signal_handler).unwrap();
+}
+
+#[cfg(target_os = "redox")]
+fn main() {
+    let builtins = Builtin::map();
+    let (sigint_tx, sigint_rx) = mpsc::channel();
+    let mut shell = Shell::new(&builtins, sigint_rx);
+    shell.evaluate_init_file();
+
+    if "1" == shell.variables.get_var_or_empty("HISTORY_FILE_ENABLED") {
+        shell.context.history.set_file_name(shell.variables.get_var("HISTORY_FILE"));
+        match shell.context.history.load_history() {
+            Ok(()) => {
+                // pass
+            }
+            Err(ref err) if err.kind() == ErrorKind::NotFound => {
+                let history_filename = shell.variables.get_var_or_empty("HISTORY_FILE");
+                let _ = writeln!(stderr(), "ion: failed to find history file {}: {}", history_filename, err);
+            },
+            Err(err) => {
+                let _ = writeln!(stderr(), "ion: failed to load history: {}", err);
+            }
+        }
+    }
+    shell.execute();
 }
