f16d4aef1bde530e47e956cf2c28f775a15a7b39
Michael Aaron Murphy <mmstickman@gmail.com>
2017-06-21 15:44:34 -0400

    Fix Issue (#320)

--- src/shell/completer.rs
+++ src/shell/completer.rs
@@ -66,7 +66,7 @@ fn escape(input: &str) -> String {
     let mut output = Vec::with_capacity(input.len());
     for character in input.bytes() {
         match character {
-            b'(' | b')' | b'[' | b']' => output.push(b'\\'),
+            b'(' | b')' | b'[' | b']' | b'&' => output.push(b'\\'),
             _ => ()
         }
         output.push(character);
