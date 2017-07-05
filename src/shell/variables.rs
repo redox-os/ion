3b106051ca4373433375ffee702acba7d2f72a28
Jean-Loup 'clippix' Bogalho <clippix@lse.epita.fr>
2017-07-05 23:30:04 +0200

    Enable history file by default
    Set HISTORY_FILE_ENABLED to "1" so that ion writes history by default.

--- src/shell/variables.rs
+++ src/shell/variables.rs
@@ -29,7 +29,7 @@ impl Default for Variables {
         );
         map.insert("DIRECTORY_STACK_SIZE".into(), "1000".into());
         map.insert("HISTORY_SIZE".into(), "1000".into());
-        map.insert("HISTORY_FILE_ENABLED".into(), "0".into());
+        map.insert("HISTORY_FILE_ENABLED".into(), "1".into());
         map.insert("HISTORY_FILE_SIZE".into(), "1000".into());
         map.insert("PROMPT".into(), "\x1B\']\'0;${USER}: ${PWD}\x07\x1B\'[\'0m\x1B\'[\'1;38;5;85m${USER}\x1B\'[\'37m:\x1B\'[\'38;5;75m${PWD}\x1B\'[\'37m#\x1B\'[\'0m ".into());
 
