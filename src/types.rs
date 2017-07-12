fde0dd7b06d4cba5cef786ea82e4bfb7a8d14e90
Hunter Goldstein <hunter.d.goldstein@gmail.com>
2017-07-12 13:00:28 -0400

    Document 'array\!' behavior

--- src/types.rs
+++ src/types.rs
@@ -8,6 +8,23 @@ pub type Value = String;
 pub type VariableContext = FnvHashMap<Identifier, Value>;
 pub type ArrayVariableContext = FnvHashMap<Identifier, Array>;
 
+/// Construct a new Array containing the given artuments
+///
+/// `array!` acts like the standard library's `vec!` macro, and can be thought
+/// of as a shorthand for:
+/// ```ignore,rust
+/// Array::from_vec(vec![...])
+/// ```
+/// Additionally it will call `Into::into` on each of its members so that one
+/// can pass in a vector of static string, string slices, etc., and they will
+/// automatically be converted to owned SmallStrings.
+/// ```
+/// let verbose = Array::from_vec(vec!["foo".into(), "bar".into(),
+///                                    "baz".into(), "zar".into(),
+///                                    "doz".into()]);
+/// let concise = array!["foo", "bar", "baz", "zar", "doz"];
+/// assert_eq!(verbose, concise);
+/// ```
 #[macro_export]
 macro_rules! array [
     ( $($x:expr), *) => ({
