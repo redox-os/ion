ce5a57e7fd6f6e9d8f5e84725df6c935644ef0cc
Jeremy Soller <jackpot51@gmail.com>
2017-08-11 07:36:23 -0600

    Fix redox namespaces plugin

--- src/shell/plugins/namespaces/redox.rs
+++ src/shell/plugins/namespaces/redox.rs
@@ -1,16 +1,17 @@
+use fnv::FnvHashMap;
+
 use types::Identifier;
 use super::NamespaceError;
 
-pub struct StringNamespace {}
+pub struct StringNamespace;
 
 impl StringNamespace {
-    pub fn new() -> Result<StringNamespace, NamespaceError> { Err(NamespaceError::FunctionMissing) }
+    pub fn new() -> Result<StringNamespace, NamespaceError> { Ok(StringNamespace) }
 
     pub fn execute(&self, function: Identifier) -> Result<Option<String>, NamespaceError> { Ok(None) }
 }
 
 pub fn collect() -> FnvHashMap<Identifier, StringNamespace> {
     eprintln!("ion: Redox doesn't support plugins yet");
-    let mut hashmap = FnvHashMap::default();
-    hashmap
+    FnvHashMap::default()
 }
