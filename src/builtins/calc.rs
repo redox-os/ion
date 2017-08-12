f9a4cb042d049476cb73119a5db66e65bf775a85
glowing-chemist <olie.smit0@gmail.com>
2017-08-12 12:59:06 +0100

    upgraded calc to 0.3 and added reverse polish notation evaluation support to calc

--- src/builtins/calc.rs
+++ src/builtins/calc.rs
@@ -1,20 +1,30 @@
 extern crate calc;
 
-use calc::{CalcError, eval};
+use calc::{CalcError, Value,  eval, eval_polish};
 use std::io::{self, Write};
+
+fn calc_or_polish_calc(args: String) -> Result<Value, CalcError>{
+    match eval(&args) {
+        Ok(t)  => Ok(t),
+        Err(_) => eval_polish(&args)
+    }
+}
 
 pub fn calc(args: &[&str]) -> Result<(), String> {
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
     if !args.is_empty() {
-        let result = eval(&args.join(""))?;
-        writeln!(stdout, "{}", result).map_err(CalcError::IO)?;
+        let result = calc_or_polish_calc(args.join(" "));
+        match result {
+            Ok(v)  => writeln!(stdout, "{}", v),
+            Err(e) => writeln!(stdout, "{}", e)
+        };
     } else {
         let prompt = b"[]> ";
         loop {
-            let _ = stdout.write(prompt).map_err(CalcError::IO)?;
+            let _ = stdout.write(prompt);
             let mut input = String::new();
-            io::stdin().read_line(&mut input).map_err(CalcError::IO)?;
+            io::stdin().read_line(&mut input);
             if input.is_empty() {
                 break;
             } else {
@@ -22,7 +32,11 @@ pub fn calc(args: &[&str]) -> Result<(), String> {
                     "" => (),
                     "exit" => break,
                     s => {
-                        writeln!(stdout, "{}", eval(s)?).map_err(CalcError::IO)?;
+                        let result = calc_or_polish_calc(s.to_string());
+                        match result {
+                            Ok(v)  => writeln!(stdout, "{}", v),
+                            Err(e) => writeln!(stdout, "{}", e)
+                        };
                     }
                 }
             }
