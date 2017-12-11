e9eb6d087d0aaac8e9b25e467a9a609d43920a6e
Michael Aaron Murphy <mmstickman@gmail.com>
2017-12-10 19:08:27 -0500

    Fix Issues w/ Multiline Array Assignments
    This fix will ensure that comments within multi-line array assignments are properly
    handled within both the REPL and script executions.
    
    Closes #642

--- src/shell/binary/terminate.rs
+++ src/shell/binary/terminate.rs
@@ -11,8 +11,26 @@ pub(crate) fn terminate_script_quotes<I: Iterator<Item = String>>(
         while !buffer.is_terminated() {
             loop {
                 if let Some(command) = lines.next() {
-                    buffer.append(&command);
-                    break;
+                    if !command.starts_with('#') {
+                        let mut start = 0;
+                        let cmd: &str = loop {
+                            if start >= command.len() {
+                                break &command;
+                            }
+
+                            match command[start..].find('#').map(|x| x + start) {
+                                Some(pos) if command.as_bytes()[pos-1] != b' ' => {
+                                    start = pos + 1;
+                                }
+                                Some(pos) => {
+                                    break &command[..pos]
+                                }
+                                None => break &command
+                            }
+                        };
+                        buffer.append(cmd);
+                        break;
+                    }
                 } else {
                     eprintln!("ion: unterminated quote in script");
                     return FAILURE;
@@ -40,7 +58,9 @@ pub(crate) fn terminate_quotes(shell: &mut Shell, command: String) -> Result<Str
     shell.flow_control.level += 1;
     while !buffer.is_terminated() {
         if let Some(command) = shell.readln() {
-            buffer.append(&command);
+            if !command.starts_with('#') {
+                buffer.append(&command);
+            }
         } else {
             return Err(());
         }
