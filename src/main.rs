d93ee723c322e429f4fc5ba452251f315fe0fb45
jD91mZM2 <me@krake.one>
2018-01-17 20:31:58 +0100

    Use arg_parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+extern crate arg_parser;
 extern crate ion_shell;
 extern crate smallvec;
 
+use arg_parser::ArgParser;
 use ion_shell::{flags::NO_EXEC, Binary, JobControl, ShellBuilder, MAN_ION};
 use smallvec::SmallVec;
 use std::{
@@ -14,42 +16,48 @@ fn main() {
         .set_unique_pid()
         .as_binary();
 
-    let mut args = env::args().skip(1);
-    while let Some(path) = args.next() {
-        match path.as_str() {
-            "-n" | "--no-execute" => {
-                shell.flags |= NO_EXEC;
-                continue;
-            }
-            "-c" => shell.execute_arguments(args),
-            "-v" | "--version" => shell.display_version(),
-            "-h" | "--help" => {
-                let stdout = stdout();
-                let mut stdout = stdout.lock();
-                match stdout
-                    .write_all(MAN_ION.as_bytes())
-                    .and_then(|_| stdout.flush())
-                {
-                    Ok(_) => return,
-                    Err(err) => panic!("{}", err.description().to_owned()),
-                }
-            }
-            _ => {
-                let mut array = SmallVec::from_iter(Some(path.clone().into()));
-                for arg in args {
-                    array.push(arg.into());
-                }
-                shell.variables.set_array("args", array);
-                if let Err(err) = shell.execute_script(&path) {
-                    eprintln!("ion: {}", err);
-                }
-            }
-        }
+    let mut args = ArgParser::new(4)
+        .add_flag(&["h", "help"])
+        .add_flag(&["n", "no-execute"])
+        .add_flag(&["v", "version"])
+        .add_opt("c", "command");
 
-        shell.wait_for_background();
-        let previous_status = shell.previous_status;
-        shell.exit(previous_status);
+    args.parse(env::args());
+
+    if args.found("no-execute") {
+        shell.flags |= NO_EXEC;
     }
 
-    shell.execute_interactive();
+    if args.found("help") {
+        let stdout = stdout();
+        let mut stdout = stdout.lock();
+        stdout
+            .write_all(MAN_ION.as_bytes())
+            .and_then(|_| stdout.flush())
+            .unwrap();
+    } else if args.found("version") {
+        shell.display_version();
+    } else if let Some(script) = args.get_opt("command") {
+        if let Err(err) = shell.execute_command(script) {
+            eprintln!("ion: failed to execute command: {}", err);
+        }
+    } else {
+        let mut drain = args.args.drain(..);
+        if let Some(path) = drain.next() {
+            let mut array = SmallVec::from_iter(Some(path.clone().into()));
+            for arg in drain {
+                array.push(arg.into());
+            }
+            shell.variables.set_array("args", array);
+            if let Err(err) = shell.execute_script(&path) {
+                eprintln!("ion: {}", err);
+            }
+
+            shell.wait_for_background();
+            let previous_status = shell.previous_status;
+            shell.exit(previous_status);
+        } else {
+            shell.execute_interactive();
+        }
+    }
 }
