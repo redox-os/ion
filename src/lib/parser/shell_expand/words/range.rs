c76a4a4ca914caa06632c52db254f93457602754
Roland Kovacs <zen3ger@gmail.com>
2018-02-23 13:04:43 +0100

    Fix doctests
    Add missing `ignore` to doctest of private modules.

--- src/lib/parser/shell_expand/words/range.rs
+++ src/lib/parser/shell_expand/words/range.rs
@@ -48,7 +48,7 @@ impl Range {
     /// Returns the bounds of this range as a tuple containing:
     /// - The starting point of the range
     /// - The length of the range
-    /// ```
+    /// ```ignore,rust
     /// let vec = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
     /// let range = Range::exclusive(Index::new(1), Index::new(5));
     /// let (start, size) = range.bounds(vec.len()).unwrap();
