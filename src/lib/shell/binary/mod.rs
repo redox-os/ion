b845c60da2d1fa6746423d966301fad33eac1dce
stratact <stratact1@gmail.com>
2018-05-25 09:36:31 -0700

    Have the Batching iterator moved to its own module

--- src/lib/shell/binary/mod.rs
+++ src/lib/shell/binary/mod.rs
@@ -11,6 +11,7 @@ use self::{
 };
 use super::{flow_control::Statement, status::*, FlowLogic, Shell, ShellHistory};
 use liner::{Buffer, Context};
+use iter::Batching;
 use std::{env, fs::File, io::ErrorKind, iter, path::Path, process};
 
 pub const MAN_ION: &'static str = r#"NAME
@@ -228,42 +229,4 @@ fn word_divide<'a>(buf: &'a Buffer) -> Vec<(usize, usize)> {  // -> impl Iterato
         item.ok()
     })
     .collect()
-}
-
-pub trait Batching<B, I, F>
-    where I: Iterator,
-          F: FnMut(&mut I) -> Option<B>,
-{
-    fn batching(self, func: F) -> BatchingIter<I, F>;
-}
-
-#[derive(Clone)]
-pub struct BatchingIter<I, F> {
-    iter: I,
-    func: F,
-}
-
-impl<B, I, F> Batching<B, I, F> for I
-    where I: Iterator,
-          F: FnMut(&mut I) -> Option<B>,
-{
-    fn batching(self, func: F) -> BatchingIter<I, F> {
-        BatchingIter { iter: self, func: func }
-    }
-}
-
-impl<B, I, F> Iterator for BatchingIter<I, F>
-    where I: Iterator,
-          F: FnMut(&mut I) -> Option<B>,
-{
-    type Item = B;
-    #[inline]
-    fn next(&mut self) -> Option<B> {
-        (self.func)(&mut self.iter)
-    }
-
-    #[inline]
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        (0, None)
-    }
 }
