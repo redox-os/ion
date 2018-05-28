61188c6bb59c802ac7704edd825d5e8648794975
stratact <stratact1@gmail.com>
2018-05-28 06:29:18 -0700

    Pass iterators in `PipelineExecution::exec_external` instead of Vec slices

--- src/lib/shell/job.rs
+++ src/lib/shell/job.rs
@@ -198,11 +198,6 @@ fn collect_args(args: &[String]) -> SmallVec<[&str; 16]> {
         .collect::<SmallVec<[&str; 16]>>()
 }
 
-fn iter_args<'a>(args: &'a [String]) -> impl ExactSizeIterator<Item = &'a str> {
-    args.iter()
-        .map(|x| x as &str)
-}
-
 impl RefinedJob {
     /// Returns a long description of this job: the commands and arguments
     pub(crate) fn long(&self) -> String {
@@ -238,8 +233,7 @@ impl RefinedJob {
                 ref stdout,
                 ref stderr,
             } => {
-                let args = collect_args(&args[1..]);
-                shell.exec_external(&name, &args, stdin, stdout, stderr)
+                shell.exec_external(&name, args[1..].iter().map(|s| s.as_ref()), stdin, stdout, stderr)
             }
             RefinedJob::Builtin {
                 main,
@@ -258,8 +252,7 @@ impl RefinedJob {
                 ref stdout,
                 ref stderr,
             } => {
-                let mut args = iter_args(&args);
-                shell.exec_function(name, &mut args, stdout, stderr, stdin)
+                shell.exec_function(name, &mut args.iter(), stdout, stderr, stdin)
             }
             _ => panic!("exec job should not be able to be called on Cat or Tee jobs"),
         }
