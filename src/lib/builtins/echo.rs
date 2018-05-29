8c53097bc1ded246a33b8ef6eb5deba6394c4513
stratact <stratact1@gmail.com>
2018-05-29 03:01:21 -0700

    Use SmallVec instead of Vec for echo builtin

--- src/lib/builtins/echo.rs
+++ src/lib/builtins/echo.rs
@@ -1,4 +1,5 @@
 use std::io::{self, BufWriter, Write};
+use smallvec::SmallVec;
 
 bitflags! {
     struct Flags : u8 {
@@ -10,7 +11,7 @@ bitflags! {
 
 pub(crate) fn echo(args: &[&str]) -> Result<(), io::Error> {
     let mut flags = Flags::empty();
-    let mut data: Vec<&str> = vec![];
+    let mut data: SmallVec<[&str; 16]> = SmallVec::with_capacity(16);
 
     for arg in args {
         match *arg {
