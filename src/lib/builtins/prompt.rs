6f4d458613840cfb880a32ec660ec782103ff43d
stratact <stratact1@gmail.com>
2018-05-31 23:21:57 -0700

    Get colors working

--- src/lib/builtins/prompt.rs
+++ src/lib/builtins/prompt.rs
@@ -1,4 +1,4 @@
-use shell::Shell;
+use shell::{colors::COLORS, Shell};
 use std::io::{self, Write, StdoutLock};
 
 trait AskColor {
@@ -7,11 +7,15 @@ trait AskColor {
 
 impl<'a> AskColor for StdoutLock<'a> {
     fn ask_color_for(&mut self, value: &str, new_prompt: &mut String) {
-        let _ = self.write(b"What color you would like to use for this variable?:\n");
+        let _ = self.write(b"Choose a color for the variable:\n");
+        for &color in COLORS.keys {
+            let _ = write!(self, "{}, ", color);
+        }
+        let _ = writeln!(self, "or default");
         let _ = self.flush();
         let mut input = String::new();
         let _ = io::stdin().read_line(&mut input);
-        new_prompt.push_str(&input);
+        new_prompt.push_str(&["${c::", input.trim(), "}"].concat());
         new_prompt.push_str(value);
     }
 }
@@ -19,17 +23,17 @@ impl<'a> AskColor for StdoutLock<'a> {
 pub(crate) fn prompt(args: &[String], shell: &mut Shell) -> Result<(), String> {
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
-    let options_list = b"Select the variables on how you want to customize your prompt:\n\
-                         a) USER\n\
-                         b) PWD\n\
-                         c) SWD\n\
-                         For example: \'$a@$b\' becomes \'user@/path/to/pwd\' as the prompt";
+    let options_list = format!("Select the variables on how you want to customize your prompt:\n\
+                                a) USER\n\
+                                b) PWD\n\
+                                c) SWD\n\
+                                For example: \'$a:$b>\' becomes \'{}:{}>\' as the prompt\n", shell.get_var("USER").unwrap(), shell.get_var("PWD").unwrap());
 
-    if args.is_empty() {
+    if args.len() == 1 {
         let _ = writeln!(stdout, "This part is unimplemented, it will list the different set of prompts to choose from.\n\
                                   For now use `prompt config`");
-    } else if args[0] == "config" {
-        let _ = stdout.write(options_list);
+    } else if args[1] == "config" {
+        let _ = writeln!(&mut stdout, "{}", options_list);
         let _ = stdout.flush();
         let mut input = String::new();
         let _ = io::stdin().read_line(&mut input);
@@ -37,7 +41,7 @@ pub(crate) fn prompt(args: &[String], shell: &mut Shell) -> Result<(), String> {
             let _ = writeln!(stdout, "Nothing inputted. Going back to Ion shell");
         } else {
             let mut new_prompt = String::new();
-            let mut input_iter = input.trim().chars();
+            let mut input_iter = input.trim().chars().peekable();
             while let Some(character) = input_iter.next() {
                 match character {
                     '$' => {
@@ -45,7 +49,16 @@ pub(crate) fn prompt(args: &[String], shell: &mut Shell) -> Result<(), String> {
                             Some('a') => { stdout.ask_color_for("${USER}", &mut new_prompt); }
                             Some('b') => { stdout.ask_color_for("${PWD}", &mut new_prompt); }
                             Some('c') => { stdout.ask_color_for("${SWD}", &mut new_prompt); }
-                            _ => continue,
+                            _ => (),
+                        }
+                    }
+                    '@' => {
+                        new_prompt.push('@');
+                        match input_iter.peek() {
+                            Some('$') => {
+                                let _ = writeln!(stdout, "Invalid use of '@' character, do not use them right before a variables, please try again");
+                            }
+                            _ => (),
                         }
                     }
                     character => {
@@ -53,6 +66,10 @@ pub(crate) fn prompt(args: &[String], shell: &mut Shell) -> Result<(), String> {
                     }
                 }
             }
+            if !new_prompt.ends_with(" ") {
+                new_prompt.push(' ');
+            }
+            new_prompt.push_str("${c::default}");
             shell.set_var("PROMPT", &new_prompt);
         }
     }
