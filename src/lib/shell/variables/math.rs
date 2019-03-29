b7d9d5b9ddae3fbb5a25e2cf5cd87138014c9011
Xavier L'Heureux <xavier.lheureux@icloud.com>
2019-03-29 18:39:48 -0400

    Exponents

--- src/lib/shell/variables/math.rs
+++ src/lib/shell/variables/math.rs
@@ -1,5 +1,6 @@
 use super::{super::types, Value};
 use itertools::Itertools;
+use std::ops::{Add, Div, Mul, Sub};
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum OpError {
@@ -7,12 +8,27 @@ pub enum OpError {
     ParseError(std::num::ParseFloatError),
 }
 
+pub trait Pow<RHS = Self> {
+    type Output;
+
+    fn pow(self, power: RHS) -> Self::Output;
+}
+
 macro_rules! math {
-    ($trait:ident, $fn:ident, $op:expr) => {
-        math!($trait, $fn, $op, false);
+    ($trait:ident, $fn:ident) => {
+        math!($trait, $fn, false);
     };
-    ($trait:ident, $fn:ident, $op:expr, $allfloat:expr) => {
-        impl<'a> std::ops::$trait for &'a Value {
+    ($trait:ident, $fn:ident, $allfloat:expr) => {
+        math!(
+            $trait,
+            $fn,
+            |lhs: f64, rhs: f64| { lhs.$fn(rhs) },
+            |lhs: i128, rhs: i128| { lhs.$fn(rhs) },
+            true
+        );
+    };
+    ($trait:ident, $fn:ident, $op_f_f:expr, $op_i_i:expr, $allfloat:expr) => {
+        impl<'a> $trait for &'a Value {
             type Output = Result<Value, OpError>;
 
             fn $fn(self, rhs: Self) -> Self::Output {
@@ -20,14 +36,14 @@ macro_rules! math {
                     if $allfloat {
                         rhs.parse::<f64>()
                             .map_err(OpError::ParseError)
-                            .and_then(|rhs| $op(self, rhs))
+                            .and_then(|rhs| self.$fn(rhs))
                     } else {
                         if let Ok(rhs) = rhs.parse::<i128>() {
-                            $op(self, rhs)
+                            self.$fn(rhs)
                         } else {
                             rhs.parse::<f64>()
                                 .map_err(OpError::ParseError)
-                                .and_then(|rhs| $op(self, rhs))
+                                .and_then(|rhs| self.$fn(rhs))
                         }
                     }
                 } else {
@@ -36,7 +52,7 @@ macro_rules! math {
             }
         }
 
-        impl<'a> std::ops::$trait<i128> for &'a Value {
+        impl<'a> $trait<i128> for &'a Value {
             type Output = Result<Value, OpError>;
 
             fn $fn(self, rhs: i128) -> Self::Output {
@@ -45,15 +61,15 @@ macro_rules! math {
                         if $allfloat {
                             lhs.parse::<f64>()
                                 .map_err(OpError::ParseError)
-                                .map(|lhs| $op(lhs, rhs as f64).to_string())
+                                .map(|lhs| $op_f_f(lhs, rhs as f64).to_string())
                                 .map(|v| Value::Str(v.into()))
                         } else {
                             lhs.parse::<i128>()
-                                .map(|lhs| $op(lhs, rhs).to_string())
+                                .map(|lhs| $op_i_i(lhs, rhs).to_string())
                                 .or_else(|_| {
                                     lhs.parse::<f64>()
                                         .map_err(OpError::ParseError)
-                                        .map(|lhs| $op(lhs, rhs as f64).to_string())
+                                        .map(|lhs| $op_f_f(lhs, rhs as f64).to_string())
                                 })
                                 .map(|v| Value::Str(v.into()))
                         }
@@ -62,7 +78,7 @@ macro_rules! math {
                         .iter()
                         // When array will contain values insted of strings, clone will no longer be
                         // needed
-                        .map(|el| $op(&Value::Str(el.clone()), rhs))
+                        .map(|el| Value::Str(el.clone()).$fn(rhs))
                         .map_results(|result| {
                             if let Value::Str(res) = result {
                                 res
@@ -77,7 +93,7 @@ macro_rules! math {
             }
         }
 
-        impl<'a> std::ops::$trait<f64> for &'a Value {
+        impl<'a> $trait<f64> for &'a Value {
             type Output = Result<Value, OpError>;
 
             fn $fn(self, rhs: f64) -> Self::Output {
@@ -85,13 +101,13 @@ macro_rules! math {
                     Value::Str(lhs) => lhs
                         .parse::<f64>()
                         .map_err(OpError::ParseError)
-                        .map(|lhs| $op(lhs, rhs).to_string())
+                        .map(|lhs| $op_f_f(lhs, rhs).to_string())
                         .map(|v| Value::Str(v.into())),
                     Value::Array(lhs) => lhs
                         .iter()
                         // When array will contain values insted of strings, clone will no longer be
                         // needed
-                        .map(|el| $op(&Value::Str(el.clone()), rhs))
+                        .map(|el| Value::Str(el.clone()).$fn(rhs))
                         .map_results(|result| {
                             if let Value::Str(res) = result {
                                 res
@@ -108,7 +124,14 @@ macro_rules! math {
     };
 }
 
-math!(Add, add, |lhs, rhs| { lhs + rhs });
-math!(Sub, sub, |lhs, rhs| { lhs - rhs });
-math!(Mul, mul, |lhs, rhs| { lhs * rhs });
-math!(Div, div, |lhs, rhs| { lhs / rhs }, true);
+math!(Add, add);
+math!(Sub, sub);
+math!(Mul, mul);
+math!(Div, div, true);
+math!(
+    Pow,
+    pow,
+    |lhs: f64, rhs: f64| { lhs.powf(rhs) },
+    |lhs: i128, rhs: i128| { lhs.pow(rhs as u32) },
+    true
+);
