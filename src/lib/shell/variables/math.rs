4b8c437f06c287317a1822f806297fe9ab1268aa
Xavier L'Heureux <xavier.lheureux@icloud.com>
2019-03-29 20:35:46 -0400

    Add checks for arithmetic

--- src/lib/shell/variables/math.rs
+++ src/lib/shell/variables/math.rs
@@ -5,6 +5,7 @@ use std::ops::{Add, Div, Mul, Sub};
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum OpError {
     TypeError,
+    CalculationError,
     ParseError(std::num::ParseFloatError),
 }
 
@@ -15,17 +16,8 @@ pub trait Pow<RHS = Self> {
 }
 
 macro_rules! math {
-    ($trait:ident, $fn:ident) => {
-        math!($trait, $fn, false);
-    };
-    ($trait:ident, $fn:ident, $allfloat:expr) => {
-        math!(
-            $trait,
-            $fn,
-            |lhs: f64, rhs: f64| { lhs.$fn(rhs) },
-            |lhs: i128, rhs: i128| { lhs.$fn(rhs) },
-            true
-        );
+    ($trait:ident, $fn:ident, $op_f_f:expr, $op_i_i:expr) => {
+        math!($trait, $fn, $op_f_f, $op_i_i, false);
     };
     ($trait:ident, $fn:ident, $op_f_f:expr, $op_i_i:expr, $allfloat:expr) => {
         impl<'a> $trait for &'a Value {
@@ -57,23 +49,22 @@ macro_rules! math {
 
             fn $fn(self, rhs: i128) -> Self::Output {
                 match self {
-                    Value::Str(lhs) => {
-                        if $allfloat {
+                    Value::Str(lhs) => if $allfloat {
+                        lhs.parse::<f64>()
+                            .map_err(OpError::ParseError)
+                            .map(|lhs| $op_f_f(lhs, rhs as f64).to_string())
+                    } else {
+                        if let Ok(lhs) = lhs.parse::<i128>() {
+                            $op_i_i(lhs, rhs)
+                                .ok_or(OpError::CalculationError)
+                                .map(|result| result.to_string())
+                        } else {
                             lhs.parse::<f64>()
                                 .map_err(OpError::ParseError)
                                 .map(|lhs| $op_f_f(lhs, rhs as f64).to_string())
-                                .map(|v| Value::Str(v.into()))
-                        } else {
-                            lhs.parse::<i128>()
-                                .map(|lhs| $op_i_i(lhs, rhs).to_string())
-                                .or_else(|_| {
-                                    lhs.parse::<f64>()
-                                        .map_err(OpError::ParseError)
-                                        .map(|lhs| $op_f_f(lhs, rhs as f64).to_string())
-                                })
-                                .map(|v| Value::Str(v.into()))
                         }
                     }
+                    .map(|v| Value::Str(v.into())),
                     Value::Array(lhs) => lhs
                         .iter()
                         // When array will contain values insted of strings, clone will no longer be
@@ -124,14 +115,27 @@ macro_rules! math {
     };
 }
 
-math!(Add, add);
-math!(Sub, sub);
-math!(Mul, mul);
-math!(Div, div, true);
+math!(Add, add, |lhs: f64, rhs: f64| { lhs.add(rhs) }, |lhs: i128, rhs: i128| {
+    lhs.checked_add(rhs)
+});
+math!(Sub, sub, |lhs: f64, rhs: f64| { lhs.sub(rhs) }, |lhs: i128, rhs: i128| {
+    lhs.checked_sub(rhs)
+});
+math!(Mul, mul, |lhs: f64, rhs: f64| { lhs.mul(rhs) }, |lhs: i128, rhs: i128| {
+    lhs.checked_mul(rhs)
+});
+math!(
+    Div,
+    div,
+    |lhs: f64, rhs: f64| { lhs.div(rhs) },
+    |lhs: i128, rhs: i128| { lhs.checked_div(rhs) },
+    true
+);
+// checked pow will only be available with version 1.34, so for now, only perform operation
 math!(
     Pow,
     pow,
     |lhs: f64, rhs: f64| { lhs.powf(rhs) },
-    |lhs: i128, rhs: i128| { lhs.pow(rhs as u32) },
+    |lhs: i128, rhs: i128| { Some(lhs.pow(rhs as u32)) },
     true
 );
