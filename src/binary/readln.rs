b44fedc9a932962abd7a9f4a26c3de4bb68f0f3e
AdminXVII <xavier.lheureux@icloud.com>
2020-01-26 06:25:59 +0000

    fix(readln): Make sure to reset the O_FLAGS when reading lines
    When some binaries set the TTY to non-blocking mode, the shell would panic because of a nonblock error. Fix this by reseting the O_FLAGS while reading lines

--- src/binary/readln.rs
+++ src/binary/readln.rs
@@ -1,11 +1,20 @@
 use super::{completer::IonCompleter, InteractiveShell};
 use ion_shell::Shell;
 use std::io::ErrorKind;
+use nix::fcntl::{OFlag, fcntl, FcntlArg};
 
 impl<'a> InteractiveShell<'a> {
+    /// Make sure to reset the fd to blocking mode
+    fn to_blocking(fd: std::os::unix::io::RawFd) {
+        fcntl(fd, FcntlArg::F_SETFL(OFlag::O_RDWR)).unwrap();
+    }
+
     /// Ion's interface to Liner's `read_line` method, which handles everything related to
     /// rendering, controlling, and getting input from the prompt.
     pub fn readln<T: Fn(&mut Shell<'_>)>(&self, prep_for_exit: &T) -> Option<String> {
+        Self::to_blocking(0);
+        Self::to_blocking(1);
+        Self::to_blocking(2);
         let prompt = self.prompt();
         let line = self.context.borrow_mut().read_line(
             prompt,
