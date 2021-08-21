e536444cce9d0a52029bfd1d572d5c362672289d
Philip Koperski <philip@pretzelwagon.com>
2021-08-21 13:29:18 -0400

    feat(builtin): Add quiet flag to math command
    Add flag to open the math command's REPL without displaying the welcome message.
    Provides cleaner terminal scrollback.

--- src/lib/builtins/math.rs
+++ src/lib/builtins/math.rs
@@ -5,7 +5,11 @@ use calc::{eval_polish_with_env, eval_with_env, CalcError, Value};
 use liner::{Context, Prompt};
 use std::io::{self, Read};
 
-const REPL_GUIDE: &str = r#"Ion's integrated calculator
+const REPL_NO_TTY_INIT_CAPACITY: usize = 1024;
+
+const QUIET_FLAG: &str = "-q";
+
+const REPL_WELCOME: &str = r#"Ion's integrated calculator
 Type in expressions to have them evaluated.
 Type "help" for help."#;
 
@@ -37,6 +41,52 @@ fn calc_or_polish_calc_with_env(
     eval_with_env(args, env).or_else(|_| eval_polish_with_env(args, env))
 }
 
+fn repl() -> Status {
+    let mut context = Context::new();
+    let mut ans = None;
+    loop {
+        match context
+            .read_line(Prompt::from("ion-math: "), None, &mut EmptyCompleter)
+            .as_ref()
+            .map(AsRef::as_ref)
+        {
+            Ok("") => return Status::SUCCESS,
+            Ok(text) if text.trim() == "exit" => return Status::SUCCESS,
+            Ok(text) if text.trim() == "help" => eprintln!("{}", REPL_HELP),
+            Ok(s) => {
+                let mut env = calc::parse::DefaultEnvironment::with_ans(ans.clone());
+                let result = calc_or_polish_calc_with_env(s, &mut env);
+                match result {
+                    Ok(v) => {
+                        println!("{}", v);
+                        ans = Some(v);
+                    }
+                    Err(e) => eprintln!("{}", e),
+                }
+            }
+            Err(err) => {
+                eprintln!("{}", err);
+                return Status::SUCCESS;
+            }
+        }
+    }
+}
+
+fn init_repl(flag: Option<&str>) -> Status {
+    if atty::is(atty::Stream::Stdin) {
+        if let Some(QUIET_FLAG) = flag {
+            repl()
+        } else {
+            println!("{}", REPL_WELCOME);
+            repl()
+        }
+    } else {
+        let mut input = String::with_capacity(REPL_NO_TTY_INIT_CAPACITY);
+        io::stdin().read_to_string(&mut input).unwrap();
+        repl()
+    }
+}
+
 #[builtin(
     desc = "Floating-point calculator",
     man = "
@@ -47,6 +97,9 @@ DESCRIPTION
     Evaluates arithmetic expressions
 
 SPECIAL EXPRESSIONS
+    -q (only in non-interactive mode)
+        opens interactive mode without welcome message
+
     help (only in interactive mode)
         prints this help text
 
@@ -74,56 +127,18 @@ AUTHOR
     Written by Hunter Goldstein."
 )]
 pub fn math(args: &[crate::types::Str], _: &mut crate::Shell<'_>) -> Status {
-    if args.get(1).is_some() {
-        let result = calc_or_polish_calc(&args[1..].join(" "));
-        match result {
-            Ok(v) => {
-                println!("{}", v);
-                Status::SUCCESS
-            }
-            Err(e) => Status::error(format!("{}", e)),
-        }
-    } else if atty::is(atty::Stream::Stdin) {
-        println!("{}", REPL_GUIDE);
-        let mut context = Context::new();
-        let mut ans = None;
-        loop {
-            match context
-                .read_line(Prompt::from("ion-math: "), None, &mut EmptyCompleter)
-                .as_ref()
-                .map(AsRef::as_ref)
-            {
-                Ok("") => return Status::SUCCESS,
-                Ok(text) if text.trim() == "exit" => return Status::SUCCESS,
-                Ok(text) if text.trim() == "help" => eprintln!("{}", REPL_HELP),
-                Ok(s) => {
-                    let mut env = calc::parse::DefaultEnvironment::with_ans(ans.clone());
-                    let result = calc_or_polish_calc_with_env(s, &mut env);
-                    match result {
-                        Ok(v) => {
-                            println!("{}", v);
-                            ans = Some(v);
-                        }
-                        Err(e) => eprintln!("{}", e),
-                    }
+    match args.get(1) {
+        Some(s) if s == "-q" => init_repl(Some(&s)),
+        Some(_) => {
+            let result = calc_or_polish_calc(&args[1..].join(" "));
+            match result {
+                Ok(v) => {
+                    println!("{}", v);
+                    Status::SUCCESS
                 }
-                Err(err) => {
-                    eprintln!("{}", err);
-                    return Status::SUCCESS;
-                }
+                Err(e) => Status::error(format!("{}", e)),
             }
         }
-    } else {
-        let mut input = String::with_capacity(1024);
-        io::stdin().read_to_string(&mut input).unwrap();
-
-        let result = calc_or_polish_calc(&input);
-        match result {
-            Ok(v) => {
-                println!("{}", v);
-                Status::SUCCESS
-            }
-            Err(e) => Status::error(format!("{}", e)),
-        }
+        None => init_repl(None),
     }
 }
