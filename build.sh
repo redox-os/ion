5a14c99b366ddae4c5656bd303c71e21109b5f7e
voidboy <aurelienclerac@pm.me>
2022-11-26 10:08:32 +0100

    remove bash scripts

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-RUSTUP=0 make # By default RUSTUP equals 1, which is for developmental purposes
-sudo make install prefix=/usr
-sudo make update-shells prefix=/usr
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test
-grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html
